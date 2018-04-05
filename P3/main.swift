d2bc469afc0d42c172f4f734b7c5253651aeeb62
Shoksbe <deknyfgregory@gmail.com>
2018-04-05 18:21:34 +0200

    Initial Commit

---
+++ P3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3
+//
+//  Created by Gregory De knyf on 5/04/18.
+//  Copyright © 2018 Gregory De knyf. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
